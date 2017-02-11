b3a9f7dfb4da187bc5738c4991c1e8bdac9dc83a
Christine Juruena <chjuruena@up.edu.ph>
2017-02-11 15:28:29 +0800

    Initial project files - UI only

---
+++ insert.js
@@ -0,0 +1,7 @@
+// var div=document.createElement("div"); 
+// document.body.appendChild(div); 
+// div.innerText="test123";
+
+alert('hello ' + document.location.href);
+
+
---
+++ popup.js
@@ -0,0 +1,41 @@
+// var app = chrome.runtime.getBackgroundPage();
+
+function hello() {
+  chrome.tabs.executeScript({
+    file: 'insert.js'
+  }); 
+}
+
+// document.addEventListener('DOMContentLoaded', function () { // working
+//     // el.addEventListener('click', swapper, false);
+// 		document.getElementById('clickme').addEventListener('click', hello);
+// });
+
+window.onload=function(){
+	document.getElementById('clickme').addEventListener('click', hello);
+
+
+
+}
+function myFunction() {
+	    // document.getElementById("myDropdown").classList.toggle("show");
+	var myDropdown = document.getElementById("myDropdown");
+	myDropdown.classList.toggle("show");
+	}
+
+
+$(function() {
+    $( "#slider-1" ).slider();
+
+	$('#flat-slider').slider({
+	  orientation: 'horizontal',
+	  range:       false,
+	  values:      [50]
+	});
+});
+
+// var clickme = document.getElementById('clickme');
+// console.log(clickme);
+// if(clickme){
+//   clickme.addEventListener('click', hello);
+// }
