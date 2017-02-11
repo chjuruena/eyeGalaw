e9574c424ed30d01d980f22b4d130f9b6d6e7cdf
Christine Juruena <chjuruena@up.edu.ph>
2017-02-12 00:22:33 +0800

    [eyeGalaw]: Experimented on adding html/ui on webpages success!

--- popup.js
+++ popup.js
@@ -1,19 +1,16 @@
 // var app = chrome.runtime.getBackgroundPage();
 
 function hello() {
-   chrome.tabs.executeScript({file: "thirdParty/webgazer.js"}, function(){
-            chrome.tabs.executeScript({file: 'insert.js'});
-        });
+   // chrome.tabs.executeScript({file: "src/thirdParty/webgazer.js"}, function(){
+   //          chrome.tabs.executeScript({file: 'insert.js'});
+   //      });
+// uncomment later
 
-  // chrome.tabs.executeScript({
-    // file: 'insert.js'
-  // }); 
+  chrome.tabs.executeScript({
+    file: 'insert.js'
+  }); 
 }
 
-// document.addEventListener('DOMContentLoaded', function () { // working
-//     // el.addEventListener('click', swapper, false);
-// 		document.getElementById('clickme').addEventListener('click', hello);
-// });
 
 window.onload=function(){
 	document.getElementById('clickme').addEventListener('click', hello);
