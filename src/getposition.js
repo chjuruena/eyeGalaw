b7c73bde335f5cd174ee03fc433021a81cce1564
chjuruena <chjuruena@up.edu.ph>
2017-04-30 03:48:48 +0800

    [eyeGalaw] Tiny Change in staring screen

--- src/getposition.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-			var obj;
-					 var arrow_down = $("#arrow_down");
-					obj={ 'position': arrow_down.position(), 'offset': arrow_down.offset() };
-					var position = arrow_down.position(); var offset = arrow_down.offset();
-					console.log( "left: " + offset.left + ", top: " + position.top );
-					}); obj; 
